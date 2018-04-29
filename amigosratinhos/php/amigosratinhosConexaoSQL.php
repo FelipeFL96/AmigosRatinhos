9f226a0d2740e095427970d4f268e12f0c62b1b4
FelipeFL96 <feliepe.fernandes.lima96@gmail.com>
2018-04-29 20:25:26 -0300

    Reorganização das pastas do projeto

--- amigosratinhos/php/amigosratinhosConexaoSQL.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Dados para conexão do usuário amigo_ratinho
-$bdServidor = 'localhost';//'127.0.0.1';
-$bdUsuario = 'amigo_ratinho';
-$bdSenha = 'chupaquemachaquesabe';
-$bdBanco = 'amigosratinhos';
-?>
